ab9aa5c76eff24334d6806b7b447b60469a138ea
Layne McNish <lmcnish14@gmail.com>
2014-08-14 09:10:26 -0600

    exercise 1 - update rollover color, background color and enabled zoom capability

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
@@ -22,10 +10,10 @@ $(document).on('ready', function() {
     map                  = new AmCharts.AmMap();
     map.pathToImages     = "http://www.amcharts.com/lib/3/images/";
     map.panEventsEnabled = true;
-    map.backgroundColor  = "#535364";
+    map.backgroundColor  = "#000000";
     map.backgroundAlpha  = 1;
-    map.zoomControl.panControlEnabled  = false;
-    map.zoomControl.zoomControlEnabled = false;
+    map.zoomControl.panControlEnabled  = true;
+    map.zoomControl.zoomControlEnabled = true;
 
     var dataProvider     = {
       mapVar: AmCharts.maps.usaHigh,
@@ -40,7 +28,7 @@ $(document).on('ready', function() {
       colorSolid: "#5EB7DE",
       selectedColor: "#5EB7DE",
       outlineColor: "#666666",
-      rollOverColor: "#88CAE7",
+      rollOverColor: "red",
       rollOverOutlineColor: "#FFFFFF"
     };
 
